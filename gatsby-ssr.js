19ae1d4c7f796537798a9a8ee4136689703445bc
nipunraj <nipunraj99@gmail.com>
2023-03-30 18:06:17 +0530

    baner installed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 export function onRenderBody({ setHeadComponents }) {
   setHeadComponents([
     <script
-      id='cookieyes'
-      type='text/javascript'
-      src='https://cdn-cookieyes.com/client_data/d80f9cec632b20d257e5a646/script.js'
+      id="cookieyes"
+      type="text/javascript"
+      src="https://cdn-cookieyes.com/client_data/ea00e6c3adef26c13ab4dacd/script.js"
     ></script>,
   ]);
 }
